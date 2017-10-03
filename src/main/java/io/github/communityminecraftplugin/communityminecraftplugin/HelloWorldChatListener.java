da938d825dd82e7a0b2ce4fb77d87c1b8260dad2
Kameramann <ullerpuller92@gmail.com>
2017-10-04 00:03:43 +0200

    Fixed pom.xml Fixed Core.java HelloWorldChatListener.java HelloWorldCommand.java

--- src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/HelloWorldChatListener.java
+++ src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/HelloWorldChatListener.java
@@ -1,20 +1,18 @@
-package com.gmail.seasonguy445.helloworldplugin;
+package io.github.communityminecraftplugin.communityminecraftplugin;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import static com.gmail.seasonguy445.helloworldplugin.Core.HELLO_WORLD;
-
 public class HelloWorldChatListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onChat(AsyncPlayerChatEvent e){
 		
-		if(e.getMessage().equalsIgnoreCase(HELLO_WORLD)){
+		if(e.getMessage().equalsIgnoreCase(Core.HELLO_WORLD)){
 			
-			e.getPlayer().sendMessage(HELLO_WORLD);
+			e.getPlayer().sendMessage(Core.HELLO_WORLD);
 			
 		}
 		
--- src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/HelloWorldCommand.java
+++ src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/HelloWorldCommand.java
@@ -1,18 +1,17 @@
-package com.gmail.seasonguy445.helloworldplugin;
+package io.github.communityminecraftplugin.communityminecraftplugin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import static com.gmail.seasonguy445.helloworldplugin.Core.HELLO_WORLD;
-
 public class HelloWorldCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender s, Command c, String l, String[] arg3) {
-		
-		s.sendMessage(HELLO_WORLD);
-		
+		if(c.getName().equalsIgnoreCase("helloworld")) {
+			s.sendMessage(Core.HELLO_WORLD);
+			return true;
+		}
 		return true;
 	}
 	

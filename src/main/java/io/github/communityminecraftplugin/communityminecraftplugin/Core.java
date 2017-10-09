cdc6c22863ef0ee44419670171476b938fb17c90
s3xi <jedkristian@live.com>
2017-10-09 16:54:07 +0100

    Add join message and configuration

--- src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/Core.java
+++ src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/Core.java
@@ -1,6 +1,14 @@
 package io.github.communityminecraftplugin.communityminecraftplugin;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import io.github.communityminecraftplugin.communityminecraftplugin.command.HelloWorldCommand;
+import io.github.communityminecraftplugin.communityminecraftplugin.configuration.Configuration;
+import io.github.communityminecraftplugin.communityminecraftplugin.configuration.Settings;
+import io.github.communityminecraftplugin.communityminecraftplugin.listener.HelloWorldChatListener;
+import io.github.communityminecraftplugin.communityminecraftplugin.listener.JoinListener;
 
 public class Core extends JavaPlugin {
 	
@@ -8,19 +16,32 @@ public class Core extends JavaPlugin {
 	
 	@Override
 	public void onEnable(){
-		
-		getLogger().info(HELLO_WORLD);
-		getCommand("helloworld").setExecutor(new HelloWorldCommand());
-		getServer().getPluginManager().registerEvents(new HelloWorldChatListener(), this);
-		
+		preInit();
+		postInit();
 	}
 	
 	@Override
 	public void onDisable(){
-		
 		getLogger().info(HELLO_WORLD.replace("Hello,", "Goodbye,"));
-		
 	}
 	
+	/**
+	 * Is called pre-initialisation.
+	 * I.e. loading configurations, etc.
+	 */
+	private void preInit() {
+		Settings.registerFromFile(new Configuration(this, new File("settings.yml")));
+	}
+	
+	/**
+	 * Is called post-initialisation.
+	 * I.e. loading objects that depend on configurations, etc.
+	 */
+	private void postInit() {
+		getLogger().info(HELLO_WORLD);
+		getCommand("helloworld").setExecutor(new HelloWorldCommand());
+		getServer().getPluginManager().registerEvents(new HelloWorldChatListener(), this);
+		getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+	}
 	
 }

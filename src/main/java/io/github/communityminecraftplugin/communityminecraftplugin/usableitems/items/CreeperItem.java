7c440e333485cc8fb680828084acd9434d220e72
monst12 <Tmc-Support@online.de>
2017-10-30 18:28:14 +0100

    Add searchItem in UsableItemManager returning an Optional of the UsableItem found. Add spawn limit of creepers flying on bats in CreeperItem. Fix missing registration of instantiated items by adding an self-registering mechanism inside the item classes. Fix not working cooldown in LeapItem.

--- src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/usableitems/items/CreeperItem.java
+++ src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/usableitems/items/CreeperItem.java
@@ -3,6 +3,7 @@ package io.github.communityminecraftplugin.communityminecraftplugin.usableitems.
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Bat;
 import org.bukkit.entity.Creeper;
@@ -25,7 +26,9 @@ import io.github.communityminecraftplugin.communityminecraftplugin.usableitems.U
  */
 public class CreeperItem extends UsableItem implements Listener
 {
-	
+
+    private static final int MAX_ALIVE = 15;
+    private int alive = 0;
 	private static final int FLY_TICKS = 20 * 5;
 	private static final PotionEffect INVISIBLE = new PotionEffect(PotionEffectType.INVISIBILITY, FLY_TICKS, 1, true, false);
 	
@@ -35,11 +38,18 @@ public class CreeperItem extends UsableItem implements Listener
 	{
 		super(Material.STICK, ChatColor.GREEN + "Throw Creeper");
 		this.plugin = plugin;
+		this.register();
 	}
 
 	@Override
 	public EventAction click(Player player, ItemStack item, Block block, Entity entity, boolean rightClick)
 	{
+	    if(this.alive >= MAX_ALIVE){
+            player.sendMessage(ChatColor.GREEN + "Creeper> " + ChatColor.GRAY +
+                    "Please wait until another " + ChatColor.GOLD + "Creeper" + ChatColor.GRAY + " blew up.");
+            return EventAction.CANCEL;
+        }
+
 		Bat bat = player.getWorld().spawn(player.getEyeLocation(), Bat.class);
 		bat.setInvulnerable(true);
 		bat.addPotionEffect(INVISIBLE);
@@ -48,15 +58,23 @@ public class CreeperItem extends UsableItem implements Listener
 		Creeper creeper = player.getWorld().spawn(player.getEyeLocation(), Creeper.class);
 		creeper.setInvulnerable(true);
 		creeper.setAI(false);
+
+		bat.addPassenger(creeper);
+
+		this.alive++;
 		
 		Bukkit.getScheduler().runTaskLater(plugin, () ->
 		{
 			bat.remove();
 			creeper.remove();
+			creeper.getWorld().playSound(creeper.getLocation(), Sound.ENTITY_CREEPER_DEATH, 10f, 10f);
 			creeper.getWorld().createExplosion(creeper.getLocation(), 0);
+
+			//This works because we are inside the same thread. Please be careful using this in another context.
+			this.alive--;
 		}, FLY_TICKS);
 		
-		return null;
+		return EventAction.NONE;
 	}
 
 }

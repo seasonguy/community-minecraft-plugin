2b410de9f54dfb3f4cf48d5f4a2579aa734b5675
s3xi <jedkristian@live.com>
2019-10-22 21:20:59 +0100

    Tidy up & include hacktoberfest URL

--- src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/listener/HacktoberfestListener.java
+++ src/main/java/io/github/communityminecraftplugin/communityminecraftplugin/listener/HacktoberfestListener.java
@@ -1,6 +1,8 @@
 package io.github.communityminecraftplugin.communityminecraftplugin.listener;
 
+import java.time.YearMonth;
 import java.util.Calendar;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,12 +12,14 @@ public class HacktoberfestListener implements Listener {
      * If its october ask the user if they have participated in Hacktoberfest
      * and let them know how many days there are left to participate
      */
-    @SuppressWarnings("MagicConstant")
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.OCTOBER) {
-            event.getPlayer().sendMessage(ChatColor.GOLD + "Have you participated in Hacktoberfest yet? There are still "
-                                          + ChatColor.GREEN + (31 - Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + ChatColor.GOLD + " days left!");
-
+            String msg = ChatColor.GOLD + "Have you participated in Hacktoberfest yet? There are still " + ChatColor.GREEN + "%s" + ChatColor.GOLD + " days left!";
+            String url = ChatColor.AQUA + "https://hacktoberfest.digitalocean.com/";
+            YearMonth yearMonthObject = YearMonth.of(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH) + 1);
+            int daysRemaining = yearMonthObject.lengthOfMonth() - Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+            event.getPlayer().sendMessage(String.format(msg, String.valueOf(daysRemaining)));
+            event.getPlayer().sendMessage(url);
         }
     }
 }
